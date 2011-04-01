2d7504b7a3e0272f57c69a096926a32447a45502
Johnny Wong <jwon145@cse.unsw.edu.au>
2011-04-01 16:17:54 +1100

    can do newline, word, byte counts and longest line length

--- uniq.pl
+++ uniq.pl
@@ -2,16 +2,54 @@
 
 @files = ();
 
+%options = ("-u", 0,
+            "-d", 0,
+            "-i", 0,
+            "-c", 0,
+            "-f", 0,
+            "-w", 0);
+
 foreach $arg (@ARGV) {
 	if ($arg eq "--version") {
-		print "$0-git\n";
+print <<ENDVERSION;
+$0 (GNU coreutils) git
+License WTFPLv2: 
+This is free software: it comes without any warranty, to
+the extent permitted by applicable law. You can redistribute it
+and/or modify it under the terms of the Do What The Fuck You Want
+To Public License, Version 2, as published by Sam Hocevar.
+
+Written by Johnny Wong and no other cs2041 students.
+ENDVERSION
 		exit(0);
 	} 
     if ($arg eq "--help") {
 print <<ENDHELP;
-Usage: $0 [OPTION]... [FILE]...
-       
+Usage: $0 [OPTION]... [INPUT [OUTPUT]]
+
+Discard all but one of successive identical lines from INPUT (or
+standard input), writing to OUTPUT (or standard output).
+
+    --help              display this help and exit
+    --version           output version information and exit
+    -u,                 only print unique lines
+
+    -i,                 ignore differences in case when comparing
+    -c,                 prefix lines by the number of occurrences
+    -d,                 only print duplicate lines
+
+    -f,                 avoid comparing the first N fields
+    -w,                 compare no more than N characters in lines
+
+A field is a run of whitespace, then non-whitespace characters.
+Fields are skipped before chars.
+
+Note: 'uniq' does not detect repeated lines unless they are adjacent.
+You may want to sort the input first, or use `sort -u' without `uniq'.
+
+Report bugs to jwon145\@cse.uns -- wait no, ignore them. They are features.
 ENDHELP
+        exit(0);
     }
 	# handle other options
 	# ...
--- wc.pl
+++ wc.pl
@@ -1,28 +1,118 @@
 #!/usr/bin/perl -w
 
+# #array with the 4 options, set to 0s
+# if all 4 are 0, use default (-lwc in that order)
+# else, use as set
+
+sub lineProcessing($);
+
+use constant {
+    LINECOUNT   => 0,
+    WORDCOUNT   => 1,
+    CHARCOUNT   => 2,
+    LONGESTLINE => 3,
+};
+
 @files = ();
+
+%options = ("-l", 0,
+            "-w", 0,
+            "-c", 0,
+            "-L", 0);
+
+#          line  word  char  long  file
+@count = ();
 
 foreach $arg (@ARGV) {
 	if ($arg eq "--version") {
-		print "$0-git\n";
+print <<ENDVERSION;
+$0 (GNU coreutils) git
+License WTFPLv2: 
+This is free software: it comes without any warranty, to
+the extent permitted by applicable law. You can redistribute it
+and/or modify it under the terms of the Do What The Fuck You Want
+To Public License, Version 2, as published by Sam Hocevar.
+
+Written by Johnny Wong and no other cs2041 students.
+ENDVERSION
 		exit(0);
-	} 
+	}
     if ($arg eq "--help") {
 print <<ENDHELP;
 Usage: $0 [OPTION]... [FILE]...
        
+Print newline, word, and byte counts for each FILE, and a total line if
+more than one FILE is specified.  With no FILE, or when FILE is -,
+read standard input.
+
+    --help      display this help and exit
+    --version   output version information and exit
+    -c,         print the byte counts
+    -w,         print the word counts
+    -l,         print the newline counts
+
+    -L,         print the length of the longest line
+
+Report bugs to jwon145\@cse.uns -- wait no, ignore them. They are features.
 ENDHELP
+        exit(0);
     }
-	# handle other options
-	# ...
+    if ($arg =~ /-([A-KM-Za-bd-km-vx-z])/) {
+        die("$0: invalid option -- '$1'\nTry `$0 --help' for more information.\n");
+    }
+    if ($arg =~ /(-[lwcL])/) {
+        $options{"$1"} = 1;
+    }
 	else {
 		push @files, $arg;
 	}
 }
 
-foreach $f (@files) {
-	open(F,"<$f") or die "$0: Can't open $f: $!\n";
-	# process F
-	#...
-	close(F);
+if (scalar(@files) == 0) {
+    ($charCount, $wordCount, $lineCount, $longestLine) = (0, 0, 0, 0);
+    while (<STDIN>) {
+        lineProcessing($_);
+    }
+    push(@count, [$lineCount, $wordCount, $charCount, $longestLine]);
+} else {
+    foreach $f (@files) {
+        open(F,"<$f") or die "$0: Can't open $f: $!\n";
+        ($charCount, $wordCount, $lineCount, $longestLine) = (0, 0, 0, 0);
+        while (<F>) {
+            lineProcessing($_);
+        }
+        push(@count, [$lineCount, $wordCount, $charCount, $longestLine, $f]);
+        close(F);
+    }
+}
+
+for $i (0..$#count) {
+    for $j (0..$#{$count[$i]}) {
+        print("$count[$i][$j] ");
+    }
+    print("\n");
+}
+
+sub lineProcessing($) {
+    my ($line) = @_;
+    my $tempCharCount = 0;
+
+    foreach $c (split(//, $line)) {
+        $tempCharCount++;
+        if ($c eq "\n") {
+            $lineCount++;
+        }
+    }
+
+    if ($tempCharCount > $longestLine) {
+        $longestLine = $tempCharCount;
+        if ($line =~ /\n/) {    # longest line doesn't count newlines; char count does
+            $longestLine--;
+        }
+    }
+    $charCount += $tempCharCount;
+
+    @words = split(' ', $line);
+    $wordCount += scalar(@words);
+    @words = ();
 }
