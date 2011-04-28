7bd34d43d42ba71cd86bd4217fc42cc38ee2260d
Johnny Wong <jwon145@cse.unsw.edu.au>
2011-04-28 17:41:49 +1000

    no longer masquerading as a GNU coreutil in --version

--- comm.pl
+++ comm.pl
@@ -80,7 +80,7 @@ sub getOpts {
 
 sub printVersion { 
 print <<ENDVERSION;
-$0 (GNU coreutils) v1.0.0
+$0 (new coreutils) v1.0.0
 License WTFPLv2: 
 This is free software: it comes without any warranty, to
 the extent permitted by applicable law. You can redistribute it
